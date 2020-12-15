b0a65a570cf93ad60cf8f4d7e90ed292dcf7f098
Apple456 <madfly@gmail.com>
2020-12-15 17:56:40 +1100

    Update select-app-pipeline.sh

--- scripts/select-app-pipeline.sh
+++ scripts/select-app-pipeline.sh
@@ -2,7 +2,7 @@
 set -euox pipefail
 
 APP_NAME="$(buildkite-agent meta-data get app-name)"
-ENV=$(environment)
+ENV=$environment
 
 echo "
   - label: Sync new relic alerts for $APP_NAME for $ENV environments
