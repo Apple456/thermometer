0ea7c480efe29d300b862df698b9238d613b24b2
Apple456 <madfly@gmail.com>
2020-12-15 17:36:18 +1100

    Update select-app-pipeline.sh

--- scripts/select-app-pipeline.sh
+++ scripts/select-app-pipeline.sh
@@ -6,10 +6,9 @@ echo "************* APPNAME $APP_NAME"
 ENV="$($1)"
 echo "************* ENV $ENV"
 
-echo "
   - label: Sync new relic alerts for $APP_NAME for $ENV environments
     command:
       - ./gradlew $APP_NAME:build $APP_NAME:syncNewRelicAlerts -Penv=$ENV -i
     retry:
       manual:
-        permit_on_passed: true"
+        permit_on_passed: true
